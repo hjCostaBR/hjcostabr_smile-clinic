d9646f20bf4bb8f68b16c6513ed31c9380860224
hjCostaBR <hjcosta@outlook.com.br>
2017-12-03 22:18:10 -0200

    orcamento

--- smile-clinic/src/class/Orcamento.h
+++ smile-clinic/src/class/Orcamento.h
@@ -1,9 +1,36 @@
+/**
+ * File: Orcamento.h
+ * Class: Orcamento
+ * Author: hjCostaBR
+ * 
+ * Define interface da classe Orcamento no sistema
+ * Smile Clinic
+ */
+#ifndef ORCAMENTO_H
+#define ORCAMENTO_H
+
+#include <string>
+#include <vector>
+
+#include "../../../date/Date.h"
+#include "pessoa/paciente/Paciente.h"
+#include "Procedimento.h"
+
+using namespace std;
+
 class Orcamento {
+
 public:
     Orcamento();
-    ~Orcamento();
+    Date getDataPagamento(void);
+    Paciente getPaciente(void);
+    vector<Procedimento> getProcedimentos();
+    double getValorTotalo(void);
 
 private:
-    pessoa::paciente::Paciente paciente;
-    
+    Date DataPagamento;
+    Paciente paciente;
+    vector<Procedimento> procedimentos;
 };
+
+#endif
